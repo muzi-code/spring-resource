a04e4832c2a2ee003abe54c375bfd17c7710011b
lifuyi7 <lifuyi8@jd.com>
2020-08-03 17:39:08 +0800

    remark push

---
+++ spring-framework-exercise/src/main/java/com/jd/nlp/dev/muzi/spring5/exercise/demo17/ProductService.java
@@ -0,0 +1,27 @@
+package com.jd.nlp.dev.muzi.spring5.exercise.demo17;
+
+
+//@Service("productService")
+public class ProductService {
+
+    private String name = "商品业务类";
+
+    private String well = "开始Spring5的炫酷之旅，书生不读四书五经！";
+
+    public void show(){
+        System.out.println(name+"\n"+well);
+    }
+
+//    @Autowired
+//    public ProductService(TmpField field){
+//        this.name = "有参数构造函数 : 依赖对象 ：" + field.getName() ;
+//    }
+
+//    public ProductService(TmpField field1, TmpField field2){
+//        this.name = "两个参数构造函数 : 依赖对象 ：" + field1.getName() ;
+//    }
+
+    public ProductService(){
+        this.name = "无参数构造函数";
+    }
+}
---
+++ spring-framework-exercise/src/main/java/com/jd/nlp/dev/muzi/spring5/exercise/demo17/RunTest.java
@@ -0,0 +1,66 @@
+package com.jd.nlp.dev.muzi.spring5.exercise.demo17;
+
+import com.jd.nlp.dev.muzi.spring5.exercise.demo01.ProductService;
+import com.jd.nlp.dev.muzi.spring5.exercise.demo01.ScanClass;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+//import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
+
+/**
+ * 开启Spring一个工程的方式
+ */
+public class RunTest {
+
+    @Test
+    public void run01(){
+        // 基于加载XML配置文件的方式，启动spring容器
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:spring5/**/demo01/spring.xml");
+        ProductService productService = (ProductService) context.getBean("productService");
+        productService.show();
+    }
+
+    @Test
+    public void run02(){
+        // 基于扫描注解的方式，启动spring容器
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext("com.jd.nlp.dev.muzi.spring5.exercise.demo01");
+        ProductService productService = (ProductService) context.getBean("productService");
+        productService.show();
+    }
+
+    public void run03(){
+        // 基本不用
+        new FileSystemXmlApplicationContext("");
+    }
+
+    @Test
+    public void test04(){
+        // springboot在启动的时候会使用这个上下文来启动spring容器。new Tomcat 嵌入式tomcat
+        // springboot 1.x.x 会有下面这个类
+//        new EmbeddedWebApplicationContext();
+    }
+
+    @Test
+    public void test01(){
+        // 基于加载XML配置文件的方式，启动spring容器
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring5/exercise/demo01/spring.xml");
+        ProductService productService = (ProductService) context.getBean("product");
+
+        context.setAllowBeanDefinitionOverriding(true);
+        context.setAllowCircularReferences(true);
+        context.refresh();
+        productService.show();
+    }
+
+    @Test
+    public void run06(){
+        // 基于扫描注解的方式，启动spring容器
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ScanClass.class);
+        ProductService productService = (ProductService) context.getBean("productService");
+        productService.show();
+    }
+}
